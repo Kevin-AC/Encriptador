bb4a2d306c95b9e49a82bb424bb330310f345fff
Kevin-AC <kevin.argumedo@gmail.com>
2023-02-11 16:13:59 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'blue/dark':'#0A3871',
+        'Lightgray':'#D8DFE8',
+        'bg':'#E5E5E5',
+        'Gray400':'#495057',
+        'Gray500':'#343A40',
+      },
+      width:{
+        '664':'664px'
+      },
+      height:{
+        '617':'617px'
+      }
+    },
+  },
+  plugins: [],
+}
